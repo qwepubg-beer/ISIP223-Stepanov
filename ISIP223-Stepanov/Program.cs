23ea649e8503bc5c302f63a0bfb92a0c8e9e49b6
qwepubg-beer <qwepubg@mail.ru>
2025-09-22 13:59:57 +0300

    Версия бэта, почти готово

--- ISIP223-Stepanov/Program.cs
+++ ISIP223-Stepanov/Program.cs
@@ -1,18 +1,40 @@
 string a = "Комсомольцы трудились день и ночь, не покладая рук, не вставая с постели. \r\nЛетом, мы с пацанами ходили в поход с ночевкой, и с собой взяли только необходимое. Картошку, палатку и Марию Ивановну. \r\nУмер М. Ю. Лермонтов на Кавказе, но любил он его не поэтому! \r\nПлюшкин навалил у себя в углу целую кучу и каждый день туда подкладывал. \r\nЛенский вышел на дуэль в панталонах. Они разошлись и раздался выстрел. \r\nДантес не стоил выеденного яйца Пушкина \r\nВо двор въехали две лошади. Это были сыновья Тараса Бульбы. \r\nОнегину нравился Байрон, поэтому он и повесил его над кроватью. \r\nГерасим поставил на пол блюдечко, и стал тыкать в него мордочкой. \r\nУ Онегина было тяжело внутри, и он пришел к Татьяне облегчиться. \r\nАндрей Болконский часто ездил поглядеть тот дуб, на который он был похож как две капли воды. \r\nЛермонтов родился у бабушки в деревне, когда его родители жили в Петербурге. \r\nГерасим налил Муме щей. \r\nБедная Лиза рвала цветы и этим кормила свою мать. \r\nХлестаков сел в бричку и крикнул: \"Гони, голубчик, в аэропорт! \" \r\nОтец Чацкого умер в детстве. \r\nВдруг Герман услыхал скрип рессор. Это была старая княгиня. \r\nУ Ростовых было три дочери: Наташа, Соня и Николай. \r\nИз всех женских прелестей у Марии Болконской были только глаза. \r\nТарас сел на коня. Конь согнулся, а потом засмеялся. \r\nДуша Татьяны полна любви и ждёт не дождётся, как бы обдать ею кого-нибудь. \r\nШел полк французов и кутузов. \r\nОнегин был богатый человек: по утрам он сидел в уборной, а потом ехал в цирк. \r\nПетр Первый соскочил с пьедестала и побежал за Евгением, громко цокая копытами. \r\nНос Гоголя наполнен глубочайшим содержанием. \r\nГлухонемой Герасим не любил сплетен и говорил только правду. \r\nТургенева не удовлетворяют ни отцы, ни дети. \r\nТакие девушки, как Ольга, уже давно надоели Онегину, да и Пушкину тоже. \r\nС Михаилом Юрьевичем Лермонтовым я познакомилась в детском саду. \r\nГерасим ел за четверых, а работал один. \r\nБазаров любил разных насекомых и делал им прививки. \r\nПугачев пожаловал шубу и лошадь со своего плеча. \r\nУ Чичикова много положительных черт: он всегда выбрит и пахнет. \r\nБазаров умер молодым человеком и сбыча его мечт не произошла. \r\nСыновья приехали к Тарасу и стали с ним знакомиться. \r\nЧичиков ехал в карете с поднятым задом. \r\nПо дороге в Богучарово Андрей Болконский, как старый дуб, расцвел и зазеленел. \r\nФамусов осуждает свою дочь за то, что Софья с самого утра и уже с мужчиной. \r\nНаташа была истинно русской натурой, очень любила природу и часто ходила на двор. \r\nГерасим бросил Татьяну и связался с Муму. \r\nГрушницкий тщательно целил в лоб, пуля оцарапала колено. \r\nПоэты XIX века были легкоранимыми людьми: их часто убивали на дуэлях. \r\nЗдесь он впервые узнал разговорную русскую речь от няни Арины Родионовны. \r\nПервые успехи Пьера Безухова в любви были плохие - он сразу женился. \r\nВ результате из Тихона вырос не мужчина, а самый настоящий овца. \r\nЯзык у Базарова был тупой, но потом заострился в спорах. \r\nМне нравится то, что с таким талантом Пушкин не побоялся стать народным поэтом. \r\nТроекуров был хотя не глуп, но немного с приветом. \r\nТак как Печорин - человек лишний, то и писать о нем - лишняя трата времени.";
-Text.Count(a);
-public class Text
-{
-    public string? a { get; set; }
-    
-    public static void Redactor(string a)
+Redactor();
+     static void Redactor()
     {
-        if (a.Length >= 100)
+        string a = Console.ReadLine();
+    if (a.Length >= 100)
+    {
+        bool flag = false;
+        while (true)
         {
-            bool flag = false;
+            List<StText> Stext = new List<StText>();
+            a=Sort(a);
+            
+            Console.WriteLine($" Количество слов: {Count(a)},количество предложений {Pred(a)}");
+            Console.WriteLine("1-Количество гласных и согласных");
+            Console.WriteLine("2-Самое короткое и самое длинное слово");
+            Console.WriteLine("3-Частота букв");
+            Console.WriteLine("4-Статистика прошлого текста");
+            Console.WriteLine("0-Конец");
+            string b = Console.ReadLine();
+            switch (b)
+            {
+                case "1": Console.WriteLine($" Количество гласных {Gl(a)} , согласных {Sg(a)}"); break;
+                case "2": Console.WriteLine($" Самое короткое слово: {Short(a)},самое длинное: {LONG(a)}"); break;
+                case "0": flag = true; break;
+                case "3": PrintDictionary<char, int>(Word(a)); break;
+                case "4":
+                    if (Stext.Count() > 0) StText.PrintStText(Stext[Stext.Count-1]);break;
+            }
+            StText Text1 = (Count(a), Sg(a), Gl(a), Short(a), LONG(a), Word(a), Pred(a));
+            Stext.Add(Text1);   
         }
-        else { Console.WriteLine("Текст слишком маленький"); }
+    
     }
-    public static int Count(string a)
+        else { Console.WriteLine("Текст слишком маленький"); }
+     }
+    static int Count(string a)
     {
         int p = 1;
         for (int i = 0; i < a.Length; i++)
@@ -23,16 +45,17 @@ public class Text
         return p;
 
     }
-    public static void Sort(string a)
+    static string Sort(string a)
     {
         List<char> Znak =new List<char> { ',' , ';',':','-'};
         for (int i = 0; i < a.Length; i++)
         {
            foreach (char s in Znak) { if (s == a[i]) a.Remove(i, 1); }
         }
+        return a;
     }
 
-    public static void SgGl(string a)
+    static int Gl(string a)
     {
         List<char> Znak = new List<char> { 'а', 'о', 'э', 'ю','у', 'е', 'и', 'я', 'ы' };
         int gl = 0;
@@ -41,8 +64,21 @@ public class Text
         {
             foreach (char s in Znak) { if (s == a[i]) gl +=1; else sg += 1; }
         }
+        return gl;
     }
-    public static void Word(string a)
+     static int Sg(string a)
+    {
+        List<char> Znak = new List<char> { 'а', 'о', 'э', 'ю', 'у', 'е', 'и', 'я', 'ы' };
+        int gl = 0;
+        int sg = 0;
+        for (int i = 0; i < a.Length; i++)
+        {
+            foreach (char s in Znak) { if (s == a[i]) gl += 1; else sg += 1; }
+        }
+        Console.WriteLine($"Гласных:{gl} Согласных:{sg}");
+        return sg;
+    }
+     static Dictionary<char,int> Word(string a)
     {
         Dictionary<char, int> dict = new Dictionary<char, int> {
     {'а', 0}, {'б', 0}, {'в', 0}, {'г', 0}, {'д', 0}, {'е', 0}, {'ё', 0},
@@ -59,8 +95,9 @@ public class Text
         {
             foreach (char s in russianLetters) { if (s == a[i]) dict[s]+=1;}
         }
+        return dict;
     }
-    public static void List(string a)
+     static int Pred(string a)
     {
         string[] List = a.Split(' ');
         List<string> H = new List<string>(List);
@@ -71,8 +108,9 @@ public class Text
             char u = i[i.Length -1]; 
             foreach (char s in Znak) { if (s == u) pr++; }
         }
+        return pr;
     }
-    public static void Short(string a)
+     static string Short(string a)
     {
         string[] List = a.Split(' ');
         List<string> H = new List<string>(List);
@@ -82,9 +120,9 @@ public class Text
         {
             shrt = (space.Length > i.Length) ? i:space;
         }
-       Console.WriteLine(shrt);
+      return shrt;
     }
-    public static void LONG(string a)
+    static string LONG(string a)
     {
         string[] List = a.Split(' ');
         List<string> H = new List<string>(List);
@@ -94,7 +132,49 @@ public class Text
         {
             shrt = (space.Length < i.Length) ? i : space;
         }
-        Console.WriteLine(shrt);
+        return shrt;
     }
+static void PrintDictionary<TKey, TValue>(Dictionary<TKey, TValue> dictionary)
+{
+    foreach (var pair in dictionary)
+    {
+        Console.WriteLine($"{pair.Key}: {pair.Value}");
+    }
+} 
 
+public class StText
+{
+    public int Count { get; set;}
+    public int Sg { get; set; }
+    public int Gl { get; set; }
+    public string Short { get; set; }
+    public string Long { get; set; }
+    public Dictionary<char,int > Dictionary { get; set; }
+    public int Pr { get; set; }
+   
+    public StText(int count, int sg, int gl, string @short, string @long, Dictionary<char, int> dictionary,int pr)
+    {
+        Count = count;
+        Pr = pr;
+        Sg = sg;
+        Gl = gl;
+        Short = @short;
+        Long = @long;
+        Dictionary = dictionary;
+    }
+        static public void PrintStText(StText a)
+    {
+        Console.WriteLine($" Количество слов: {a.Count},количество предложений {a.Pr}");
+        Console.WriteLine($" Количество гласных {a.Gl} , согласных {a.Sg}"); 
+        Console.WriteLine($" Самое короткое слово: {a.Short},самое длинное: {a.Long}"); 
+        foreach (var pair in   a.Dictionary)
+        {
+            Console.WriteLine($"{pair.Key}: {pair.Value}");
+        }
+
+    }
+    public static implicit operator StText((int, int, int, string, string, Dictionary<char, int>, int) v)
+    {
+        throw new NotImplementedException();
+    }
 }
