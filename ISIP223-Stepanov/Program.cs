202ec4215cb1079568e2bfc932a1932c0f0a21ed
qwepubg-beer <qwepubg@mail.ru>
2025-10-01 13:53:40 +0300

    добавил класс програм и мэйн

--- ISIP223-Stepanov/Program.cs
+++ ISIP223-Stepanov/Program.cs
@@ -1,71 +1,92 @@
-﻿class Person
+﻿namespace university
 {
-    private string FIO;
-    private DateOnly Birthday; 
-    private string Gender;
-
-    public Person(string fio, DateOnly birthday, string gender)
+    class Person
     {
-        FIO = fio;
-        Birthday = birthday;
-        Gender = gender;
-    }
-    public virtual void Print()
-    {
-        Console.WriteLine($"FIO: {FIO}\nBirthday: {Birthday}\nGender: {Gender} ");
-    }
-}
+        private string FIO;
+        private DateOnly Birthday;
+        private string Gender;
 
-class Student : Person
-{
-    private int StudentNumberID;
-    private bool PCExperience;
-    private int CourseNumber;
-    private string TheHealthGroup;
-
-    public Student(int id, string fio, DateOnly birthday, string gender, bool PCExperience, string TheHealthGroup, int CourseNumber)
-        : base(fio, birthday, gender)
-    {
-        StudentNumberID = id;
-        this.PCExperience = PCExperience;
-        this.CourseNumber = CourseNumber;
-        this.TheHealthGroup = TheHealthGroup;
+        public Person(string fio, DateOnly birthday, string gender)
+        {
+            FIO = fio;
+            Birthday = birthday;
+            Gender = gender;
+        }
+        public virtual void Print()
+        {
+            Console.WriteLine($"FIO: {FIO}\nBirthday: {Birthday}\nGender: {Gender} ");
+        }
     }
 
-    public override void Print()
+    class Student : Person
     {
-        Console.WriteLine("Student");
-        base.Print();
-        Console.WriteLine($"StudentNumberID: {StudentNumberID}\nPCExperience: {PCExperience}\nTheHealthGroup: {TheHealthGroup}\nCourseNumber: {CourseNumber}\n");
+        private int StudentNumberID;
+        private bool PCExperience;
+        private int CourseNumber;
+        private string TheHealthGroup;
+
+        public Student(int id, string fio, DateOnly birthday, string gender, bool PCExperience, string TheHealthGroup, int CourseNumber)
+            : base(fio, birthday, gender)
+        {
+            StudentNumberID = id;
+            this.PCExperience = PCExperience;
+            this.CourseNumber = CourseNumber;
+            this.TheHealthGroup = TheHealthGroup;
+        }
+
+        public override void Print()
+        {
+            Console.WriteLine("Student");
+            base.Print();
+            Console.WriteLine($"StudentNumberID: {StudentNumberID}\nPCExperience: {PCExperience}\nTheHealthGroup: {TheHealthGroup}\nCourseNumber: {CourseNumber}\n");
+        }
     }
-}
 
-class Teacher : Person
-{
-    private string Subject;
-    private int ExperienceYears;
-
-    public Teacher(string fio, DateOnly birthday, string gender, string subject, int PCExperience)
-        : base(fio, birthday, gender)
+    class Teacher : Person
     {
-        Subject = subject;
-        ExperienceYears = PCExperience;
-    }
+        private string Subject;
+        private int ExperienceYears;
 
-    public override void Print()
-    {
-        Console.WriteLine("Teacher");
-        base.Print();
-        Console.WriteLine($"Subject: {Subject}\nExperienceYears: {ExperienceYears}\n");
+        public Teacher(string fio, DateOnly birthday, string gender, string subject, int PCExperience)
+            : base(fio, birthday, gender)
+        {
+            Subject = subject;
+            ExperienceYears = PCExperience;
+        }
+
+        public override void Print()
+        {
+            Console.WriteLine("Teacher");
+            base.Print();
+            Console.WriteLine($"Subject: {Subject}\nExperienceYears: {ExperienceYears}\n");
+        }
     }
-}
-   class Curs:Teacher
-{
+    class Curs : Teacher
+    {
         public string Name;
         public Curs(string name, string Subject, int ExperienceYears, string FIO, DateOnly Birthday, string Gender)
-        : base(FIO, Birthday, Gender,Subject, ExperienceYears)
+        : base(FIO, Birthday, Gender, Subject, ExperienceYears)
         {
             this.Name = name;
         }
+        public override void Print()
+        {
+            Console.WriteLine("Curs:");
+            Console.WriteLine($"curs: {Name}");
+            base.Print();
+        }
+    }
+    class Program
+    {
+        void Main(string[] args)
+        {
+
+            bool flag = false;
+            while (!flag)
+            {
+
+            }
+        }
+    }
 }
 
