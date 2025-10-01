d089540a37c3cb42f30b19708f943b11ec0b3321
qwepubg-beer <qwepubg@mail.ru>
2025-10-01 13:05:34 +0300

    Нашел код в презентациях Гордова

--- ISIP223-Stepanov/Program.cs
+++ ISIP223-Stepanov/Program.cs
@@ -1,2 +1,61 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Person
+{
+    private string FIO;
+    private DateOnly Birthday; 
+    private string Gender;
+
+    public Person(string fio, DateOnly birthday, string gender)
+    {
+        FIO = fio;
+        Birthday = birthday;
+        Gender = gender;
+    }
+    public virtual void Print()
+    {
+        Console.WriteLine($"FIO: {FIO}\nBirthday: {Birthday}\nGender: {Gender} ");
+    }
+}
+
+class Student : Person
+{
+    private int StudentNumberID;
+    private bool PCExperience;
+    private int CourseNumber;
+    private string TheHealthGroup;
+
+    public Student(int id, string fio, DateOnly birthday, string gender, bool PCExperience, string TheHealthGroup, int CourseNumber)
+        : base(fio, birthday, gender)
+    {
+        StudentNumberID = id;
+        this.PCExperience = PCExperience;
+        this.CourseNumber = CourseNumber;
+        this.TheHealthGroup = TheHealthGroup;
+    }
+
+    public override void Print()
+    {
+        Console.WriteLine("Student");
+        base.Print();
+        Console.WriteLine($"StudentNumberID: {StudentNumberID}\nPCExperience: {PCExperience}\nTheHealthGroup: {TheHealthGroup}\nCourseNumber: {CourseNumber}\n");
+    }
+}
+
+class Teacher : Person
+{
+    private string Subject;
+    private int ExperienceYears;
+
+    public Teacher(string fio, DateOnly birthday, string gender, string subject, int PCExperience)
+        : base(fio, birthday, gender)
+    {
+        Subject = subject;
+        ExperienceYears = PCExperience;
+    }
+
+    public override void Print()
+    {
+        Console.WriteLine("Teacher");
+        base.Print();
+        Console.WriteLine($"Subject: {Subject}\nExperienceYears: {ExperienceYears}\n");
+    }
+}
