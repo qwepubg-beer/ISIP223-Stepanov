fcc4a1e824931735378abf3035c0bed2633718d6
qwepubg-beer <qwepubg@mail.ru>
2025-10-08 20:54:24 +0300

    добавил объекты класов изменил класы и функции класа програм

--- ISIP223-Stepanov/Program.cs
+++ ISIP223-Stepanov/Program.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Xml.Linq;
 
 namespace GamePR6
@@ -6,87 +7,89 @@ namespace GamePR6
     {
         public string NameW;
         public int  Damage;
-        public Weapon(string name, int damage)
+        public Weapon(string name_d, int block)
+
         {
-            NameW = name;
-            Damage = damage;
+            NameW = name_d;
+            Damage = block;
         }
     }
-    class Def : Weapon
+    class Def
     {
         public string NameD;
         public int Block;
-        public Def(string name_w, int damage, string name_d, int block)
-            : base(name_w, damage)
+        
+        public Def( string name_d, int block)
+            
         {
             NameD = name_d;
-            Damage = damage;
+            Block = block;
+           
         }
     }
-    class Player : Def
-    {
-        public string NameP;
-        public float Hp;
-        public float BHp;
-        public Player(string name_w, int damage, string name_d, int block, string name_p, int hp)
-            : base(name_w, damage, name_p, hp)
-        {
-            NameP = name_p;
-            Hp = hp;
-            BHp= hp;
-        }
-    }
-
-    class Enemy
+    class Person
     {
         public string Name;
-        public float Hp;
-        public int Damage;
+        public double Hp;
+        public double BHp;
+        public double Damage;
         public int Def;
         public string Type;
-        public Enemy(string name, int hp, int damage, int def, string type)
+        public Person(string name, int hp, int damage=0, int def=0, string type="None")
         {
             Name = name;
             Hp = hp;
             Damage = damage;
             Def = def;
             Type = type;
+            BHp = hp;
 
         }
     }
     class Program
     {
+        static Weapon Sword = new Weapon("Меч", 10);
+        static Weapon Axe = new Weapon("Топор", 15);
+        static Weapon bow = new Weapon("Лук", 12);
+        static Def shield = new Def("Щит", 60);
+        static Def leather_armor = new Def("Кожанка", 20);
+        static Def iron_armor = new Def("Кальчуга", 40);
+        
         static void Main(string[] args)
         {
-            for (int i = 0; i <= 10; i++)
-            {
-                if (i == 10) { }
-                else { CaseorBattle()}
-            }
+            Person Sperminov = new Person("Сперминов", 100);
+            Person Goblin = new Person("Гоблин", 50,10,10,"G");
+            Person Skelet = new Person("Скелет", 60, 10, 10, "S");
+            Person Mag = new Person("Маг", 40, 15, 10, "M");
+            Person Grifin = new Person("ГВВ",100,15,20,"G");
+            Person Kov = new Person("Ковальский", 150, 23, 24 , "S");
+            Person cpp = new Person("GordovC++", 72, 26, 11, "M");
+            Person cmm = new Person("PestovC--", 78, 16, 6, "S");
+            Random rand = new Random();
+
         }
-        void battle(Player p, Enemy e)
+        void battle(Person p, Person e)
         {
             while (p.Hp >= 0 && e.Hp >= 0)
             {
-                attack(p, e, 1);
-                attack(p, e, 2);
+                attack(p, e);
+                attack(e, p);
             }
         }
-        void attack(Player p, Enemy e, int a)
+        void attack(Person p, Person e)
         {
-            if (a == 1) {e.Hp-=defend(p.Damage,e.Def);}
-            if (a == 2) { p.Hp -= defend(e.Damage, p.Block); }
+           e.Hp-=defend(p.Damage,e.Def);
         }
-        float defend(int damage,int defend)
+        double defend(double damage,double defend)
         {
             Random random = new Random();
-            if (random.Next(1, 100) < defend)
+            if (random.Next(1, 100) < 40)
             {
-                return damage;
+                return damage/100*(100-defend);
             }
             else { return 0; }
         }
-        void CaseorBattle(Player p, Enemy e)
+        void CaseorBattle(Person p, Person e)
         {
             Random random = new Random();
             if (random.Next(1, 2) == 1)
@@ -95,34 +98,40 @@ namespace GamePR6
             }
             else
             {
-                Case();
+                Case(p);
             }
         }
-        void Case ()
+        void Case (Person p)
         {
-            List<Def> list = new List<Def>();
-            List<string> strings = new List<string>();
+            List<double> list = new List<double> { 1.1, 1.2, 1.3, 1.4 };   
             Random random = new Random();
             if (random.Next(1, 2) == 1)
             {
-                
+                p.Hp = p.BHp;
+                Console.WriteLine("Вы исцелены");
             }
             else
             {
-                
+                if (random.Next(1, 2) == 1)
+                {
+                    int a = random.Next(0, 3);
+                    p.BHp *= list[a];
+                }
+                else
+                {
+                    int a = random.Next(0, 3);
+                    p.Damage *= list[a];
+                }
             }
         }
-        void Pod(List<Def> d,List<Enemy> e)
-{
-    Def d1 = new Def("boxer gloves", 2, "boxer gloves", 10);
-    Def d2 = new Def("sword", 4, "shield", 20);
-    Def d3 = new Def("club", 4, "shield", 30);
-    Def d4 = new Def("super_streight", 6, "cloak", 40);
-    Def d5 = new Def("chess", 7, "board", 30);
-    Def d6 = new Def("granate", 12, "None", 0);
-    Def d7 = new Def("tank", 15 , "Iron", 70);
-    d.Add(d1);d.Add(d2);d.Add(d3);d.Add(d4);d.Add(d5);d.Add(d6);d.Add(d7);
+        void cb(Person p, Person e)
+        {
 
+            for (int i = 0; i <= 10; i++)
+            {
+                if (i == 10) { }
+                else { CaseorBattle(p, e); }
+            }
+        }
 }
-    }
 }
